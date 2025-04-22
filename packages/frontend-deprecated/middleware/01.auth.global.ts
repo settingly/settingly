88b6adbff6d94e9b6355428d5b33a43655d6ba78
Leo Gall <gall.dev@proton.me>
2025-04-22 09:11:16 +0200

    remove old frontend

--- packages/frontend-deprecated/middleware/01.auth.global.ts
+++
@@ -1,9 +0,0 @@
-const isProtectedRoute = createRouteMatcher(["/_(.*)"]);
-
-export default defineNuxtRouteMiddleware((to) => {
-  const { userId } = useAuth();
-
-  if (!userId.value && isProtectedRoute(to)) {
-    return navigateTo("/signin");
-  }
-});
--- packages/frontend-deprecated/shared/utils/get-organization-type.ts
+++
@@ -1,8 +0,0 @@
-export default function getOrganizationType(organizationId: string) {
-  if (organizationId.startsWith("org_")) {
-    return "organization";
-  } else if (organizationId.startsWith("user_")) {
-    return "user";
-  }
-  return "unknown";
-}
