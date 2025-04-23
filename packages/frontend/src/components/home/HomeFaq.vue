ba25c0f47cd0af1bb0303ec6e9ebdff4f82453f2
Leo Gall <gall.dev@proton.me>
2025-04-23 07:09:31 +0200

    feat: update paddings

--- packages/frontend/src/components/home/HomeFaq.vue
+++ packages/frontend/src/components/home/HomeFaq.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { CircleHelpIcon } from "lucide-vue-next";
-import { ref } from "vue";
+import { CircleHelpIcon } from 'lucide-vue-next';
+import { ref } from 'vue';
 
 interface FAQ {
   id: number;
@@ -11,38 +11,34 @@ interface FAQ {
 const faqs = ref<FAQ[]>([
   {
     id: 1,
-    header: "Is Settingly free to use?",
-    text: "At this time, Settingly is free to use. We are working on a paid plan that will be available in the future.",
+    header: 'Is Settingly free to use?',
+    text: 'At this time, Settingly is free to use. We are working on a paid plan that will be available in the future.',
   },
   {
     id: 2,
-    header: "How does Settingly stand out from other tools?",
-    text: "Nowadays, many tools out there are overly complex and hard to use. Settingly is designed be as simple as possible, while still being powerful.",
+    header: 'How does Settingly stand out from other tools?',
+    text: 'Nowadays, many tools out there are overly complex and hard to use. Settingly is designed be as simple as possible, while still being powerful.',
   },
   {
     id: 3,
-    header: "Can I self-host Settingly?",
-    text: "Yes, Settingly is open-source and can be self-hosted. You can find the source code on GitHub.",
+    header: 'Can I self-host Settingly?',
+    text: 'Yes, Settingly is open-source and can be self-hosted. You can find the source code on GitHub.',
   },
   {
     id: 4,
-    header: "How can I support Settingly?",
-    text: "You can support Settingly by starring the GitHub repository, contributing to the project or donating to the project.",
+    header: 'How can I support Settingly?',
+    text: 'You can support Settingly by starring the GitHub repository, contributing to the project or donating to the project.',
   },
 ]);
 </script>
 
 <template>
-  <section
-    class="relative z-20 overflow-hidden bg-white pt-20 pb-8 lg:pt-[120px] lg:pb-[50px]"
-  >
+  <section class="relative z-20 overflow-hidden bg-white pt-20 pb-8 lg:pt-[120px] lg:pb-[50px]">
     <div class="container mx-auto">
       <div class="flex flex-wrap -mx-4">
         <div class="w-full px-4">
           <div class="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
-            <span class="block mb-2 text-lg font-semibold text-primary">
-              FAQ
-            </span>
+            <span class="block mb-2 text-lg font-semibold text-primary"> FAQ </span>
             <h2 class="text-dark text-3xl font-bold sm:text-[40px]/[48px]">
               Frequently ask questions
             </h2>
@@ -50,7 +46,7 @@ const faqs = ref<FAQ[]>([
         </div>
       </div>
 
-      <div class="flex flex-wrap -mx-4">
+      <div class="flex flex-wrap mx-1">
         <template :key="faq.id" v-for="faq in faqs">
           <div class="w-full px-4 lg:w-1/2">
             <div class="mb-12 flex lg:mb-[70px]">
@@ -61,9 +57,7 @@ const faqs = ref<FAQ[]>([
               </div>
 
               <div class="w-full">
-                <h3
-                  class="mb-6 text-xl font-semibold text-dark sm:text-2xl lg:text-xl xl:text-2xl"
-                >
+                <h3 class="mb-6 text-xl font-semibold text-dark sm:text-2xl lg:text-xl xl:text-2xl">
                   {{ faq.header }}
                 </h3>
                 <p class="text-base leading-relaxed text-body">
