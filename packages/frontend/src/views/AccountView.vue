c1b50efffb761cecd2a32e74e1e48d4d93228ba3
Leo Gall <gall.dev@proton.me>
2025-04-23 11:47:45 +0200

    fix: reset password button loader is now shown on reset (#43)

--- packages/frontend/src/views/AccountView.vue
+++ packages/frontend/src/views/AccountView.vue
@@ -18,19 +18,19 @@
         class="flex flex-col lg:flex-row lg:items-center gap-3 lg:gap-0 justify-between mb-8 lg:mb-3"
       >
         <p class="text-sm">Change your password by requesting a password reset link.</p>
-        <button class="button bg-error" type="button" @click="resetPassword">Reset Password</button>
+        <button
+          class="button bg-error"
+          type="button"
+          @click="resetPassword"
+          v-if="!isResettingPassword"
+        >
+          Reset Password
+        </button>
+        <SharedSpinner v-else />
       </div>
       <div class="flex flex-col lg:flex-row lg:items-center gap-3 lg:gap-0 justify-between">
         <p class="text-sm">Delete your account. This action is irreversible.</p>
-        <button
-          class="button bg-error"
-          @click="deleteAccount"
-          type="button"
-          v-if="!isResettingPassword"
-        >
-          Delete Account
-        </button>
-        <SharedSpinner v-else />
+        <button class="button bg-error" @click="deleteAccount" type="button">Delete Account</button>
       </div>
     </FormsBase>
   </div>
