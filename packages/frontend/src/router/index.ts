1b88e7267b616be1601ca8cb00d4a3ad446e0927
Leo Gall <gall.dev@proton.me>
2025-05-11 13:42:34 +0200

    refactor: update routes structure

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -1,89 +1,76 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import { usePocketbaseStore } from '@/stores/usePocketbaseStore';
+
+const accountRoutes: RouteRecordRaw[] = [
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('@/views/SignUpView.vue'),
+  },
+  {
+    path: '/reset-password',
+    name: 'reset-password',
+    component: () => import('@/views/ResetPasswordView.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LoginView.vue'),
+  },
+  {
+    path: '/account',
+    name: 'account',
+    component: () => import('@/views/AccountView.vue'),
+  },
+];
+
+const projectsRoutes: RouteRecordRaw[] = [
+  {
+    path: '/projects',
+    name: 'projects',
+    component: () => import('../views/projects/ProjectsListView.vue'),
+  },
+
+  {
+    path: '/projects/:projectId/settings',
+    name: 'project settings',
+    component: () => import('@/views/projects/ProjectSettingsView.vue'),
+  },
+  {
+    path: '/projects/:projectId/files',
+    name: 'files',
+    component: () => import('@/views/projects/files/FilesListView.vue'),
+  },
+  {
+    path: '/projects/:projectId/files/:fileId',
+    name: 'update file',
+    component: () => import('@/views/projects/files/UpdateFileView.vue'),
+  },
+];
+
+const utilRoutes: RouteRecordRaw[] = [
+  {
+    path: '/:catchAll(.*)',
+    name: 'not-found',
+    component: () => import('../views/NotFoundView.vue'),
+  },
+  {
+    path: '/offline',
+    name: 'offline',
+    component: () => import('../views/BackendOfflineView.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/:catchAll(.*)',
-      name: 'not-found',
-      component: () => import('../views/NotFoundView.vue'),
-    },
-    {
-      path: '/offline',
-      name: 'offline',
-      component: () => import('../views/BackendOfflineView.vue'),
-    },
-    {
-      path: '/legal/imprint',
-      name: 'imprint',
-      component: () => import('../views/legal/ImprintView.vue'),
-    },
-
-    {
-      path: '/legal/privacy',
-      name: 'privacy',
-      component: () => import('../views/legal/PrivacyView.vue'),
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('@/views/SignUpView.vue'),
-    },
-    {
-      path: '/reset-password',
-      name: 'reset-password',
-      component: () => import('@/views/ResetPasswordView.vue'),
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/LoginView.vue'),
-    },
-    {
-      path: '/account',
-      name: 'account',
-      component: () => import('@/views/AccountView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/projects/ProjectsListView.vue'),
-    },
-    {
-      path: '/projects/:projectId/settings',
-      name: 'project settings',
-      component: () => import('@/views/projects/ProjectSettingsView.vue'),
-    },
-    {
-      path: '/projects/:projectId/files',
-      name: 'files',
-      component: () => import('@/views/projects/files/FilesListView.vue'),
-    },
-    {
-      path: '/projects/:projectId/files/:fileId',
-      name: 'update file',
-      component: () => import('@/views/projects/files/UpdateFileView.vue'),
-    },
-    {
-      path: '/docs',
-      name: 'docs home',
-      component: () => import('@/views/docs/DocumentsListView.vue'),
-    },
-    {
-      path: '/docs/:categorySlug/:pageSlug',
-      name: 'docs page',
-      component: () => import('@/views/docs/SingleDocumentView.vue'),
-    },
-  ],
+  routes: [...accountRoutes, ...projectsRoutes, ...utilRoutes],
 });
 
 router.beforeEach((to) => {
+  if (to.path === '/') {
+    return { name: 'projects' };
+  }
+
   const isProtectedRoute = to.path.startsWith('/projects') || to.path.startsWith('/account');
   const { isAuthenticated } = usePocketbaseStore();
 
